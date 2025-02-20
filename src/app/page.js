068058a68c27e12a4c44f4ec64b8cea11c90c478
AlbertAdekanye <adekanyealbert@gmail.com>
2025-02-20 16:47:46 +0100

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 import HomeHeader from '@/components/HomeHeader'
-import React from 'react'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
    <>
     <HomeHeader />
+    <div className="flex flex-col items-center mt-24">
+      <Image 
+        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" 
+        alt="Google Logo"
+        width={300} 
+        height={100} 
+        priority
+        style={{ width: 'auto' }} 
+      />
+        <HomeSearch />
+    </div>
    </>
   )
 }
